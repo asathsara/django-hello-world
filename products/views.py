81ea11387102345f97d7c09d26c7fa2eb77c642d
A S <65203960+asathsara@users.noreply.github.com>
2025-05-27 22:58:46 +0530

    create base and index templates

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product, Offer
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    Products = Product.objects.all()
+    return render(request, 'index.html', {'products': Products})
 
 def new(request):
     return HttpResponse("This is the new product page.")
